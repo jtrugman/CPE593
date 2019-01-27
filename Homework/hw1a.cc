fb3c87b9cd501625ed6a3f0194b8396020469000
Justin Trugman <jtrugman14@gmail.com>
2019-01-27 15:27:36 -0500

    HW1A Eratosthenes Sieve

---
+++ Homework/hw1a.cc
@@ -0,0 +1,45 @@
+/*
+Author: Justin Trugman
+HW1A - Eratosthenes Sieve
+
+I pledge my honor that I abided by the Stevens Honor System
+*/
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int eratosthenes (int n) {
+
+    // mark all items in array as true
+    bool prime[n];
+    for (int i = 0; i <= sizeof(prime); i++) {
+        prime[i] = true;
+    }
+
+
+    // mark primes equal to false
+    for (int i = 2; i <= sqrt(n); i++){
+        if (prime[i] == true){
+            for(int j = i * 2; j <= n; j += i){
+                prime[j] = false;
+            }
+        }
+    }
+
+
+    // print prime numbers
+     for (int i = 0; i <= sizeof(prime); i++) {
+         if (prime[i] == true)
+            cout << i << '\t';
+    }
+    cout << '\n';
+
+}
+
+
+int main (){
+    eratosthenes(100);
+
+}
