101811cd9b82ad4d4f9319c747fcd09f79dcb488
Justin Trugman <jtrugman14@gmail.com>
2019-03-24 18:25:21 -0400

    hw4b

--- Homework/hw4/hw4b.cc
+++ Homework/hw4/hw4b.cc
@@ -1,139 +1,154 @@
-#include <iostream>
+/*
+Author: Justin Trugman
+Homework 4B
+Collaborators: David Lehhman, Joe Puciloski
+
+*/
+#include<fstream>
+#include<sstream>
+#include<string>
+#include<iostream>
+
 using namespace std;
 
-class DoubleLinkedList2 {
+class DoubleLinkedList2{
 private:
-	class Node { // DoubleLinkedList2::Node
+	class Node{
 	public:
-		int data;
-		Node* next;
+		int val;
 		Node* prev;
-		Node(int v, Node* n, Node* p) : data(v), next(n), prev(p) {}
+		Node* next;
+		Node (int val, Node* prev, Node* next): val(val), prev(prev), next(next) {}
 	};
 	Node* head;
 	Node* tail;
 public:
 	DoubleLinkedList2() : head(nullptr), tail(nullptr) {}
-	~DoubleLinkedList2() {
-    for (Node* p = head; p != nullptr; ) {
-			Node* temp = p;
-			p = p->next;
-			delete temp;
-		}
-	}
-	DoubleLinkedList2(const DoubleLinkedList2& orig) { //O(n)
-		if (orig.head == nullptr) { // special case for empty lists
-			head = tail = nullptr;
-			return;
-		}
-		head = new Node(orig.head->data, nullptr, nullptr);
-		if (orig.head == orig.tail) {
-			tail = head;
-			return;
-		}
-		// guaranteed: at least 2 elements
-		Node* q = head;
-		for (Node* p = orig.head->next; p != nullptr; p = p->next) {
-			Node* temp = new Node(p->data, nullptr, q);
-			q->next = temp;
-			q = temp;
-		}
-
-	}
-	DoubleLinkedList2& operator =(const DoubleLinkedList2& orig) {
-		DoubleLinkedList2 copy(orig);
-		swap(head, copy.head);
-		swap(tail, copy.tail);
-		return *this;
-	}
-
-	
-	void addEnd(int v) { //O(1)
-		Node* temp = new Node(v, nullptr, tail);
-		if (tail == nullptr) {
-			head = tail = temp;
-		} else {
-			tail->next = temp;
-			tail = temp;
-		}
-	}
-	void addStart(int v) { //O(1)
-		Node* temp = new Node(v, head, nullptr);
-		if (head == nullptr) {
-			head = tail = temp;
-		} else {
-			head->prev = temp;
-			head = temp;
+	~DoubleLinkedList2(){
+		Node* q;
+		for (Node* p = head; p!= nullptr; p = q){
+			q = p->next;
+			delete p;
 		}
 	}
 
-	void removeStart() {
-		if (head == nullptr)
-			return;
-		if (head == tail) {
-      delete head;
-			head = tail = nullptr;
-			return;
-		}
-		Node* temp = head;
-		head = head->next;
-		head->prev = nullptr;
-		delete temp;
-	}
 
-	bool isEmpty() {
-		return head == nullptr;
-	}
-	
-	void removeEnd() {
-		if (tail == nullptr)
-			return;
-		if (head == tail) {
-			delete head;
-			head = tail = nullptr;
-			return;
-		}
-		Node* secondToLast = tail->prev;
-		delete tail;
-		secondToLast->next = nullptr;
-
-	}
-  friend ostream& operator <<(ostream& s, const DoubleLinkedList2& list) {
-    for (Node* p = list.head; p != nullptr; p = p->next)
-			s << p->data << ' ';
+	friend ostream& operator <<(ostream& s, DoubleLinkedList2 list){
+		for(Node* p = list.head; p != nullptr; p = p->next)
+			s << p->val<<',';
 		return s;
 	}
-	class Iterator {
-	private:
-		Node* current;
-	public:
-		Iterator(const DoubleLinkedList2& orig) : current(orig.head)  {}
-		bool operator !() const { return current != nullptr;  }
-		Iterator& operator ++() {
-			current = current->next;
-			return *this;
+	void addStart(int val){
+		if(head == nullptr){
+			head = tail = new Node(val, nullptr, nullptr);
+			return;
 		}
-    int operator *()const { return current->data; }
-	};
+		Node* list = new Node(val, nullptr, head);
+		head->prev = list;
+		head = list;
+	}
+	void addEnd(int val){
+		if(head == nullptr){
+			head = tail = new Node(val, nullptr, nullptr);
+			return;
+		}
+		Node* list = new Node(val, tail, nullptr);
+		tail->next = list;
+		tail = list;
+	}
+	void removeEnd(){
+		if(tail == nullptr)
+			return;
+		if(head->next == nullptr){
+			delete head;
+			head = tail = nullptr;
+		}
+		Node* list  = tail;
+		tail = tail -> prev;
+		delete list;
+		tail -> next = nullptr;
+	}
+	void removeStart(){
+		if(head == nullptr)
+			return;
+		if(head-> next == nullptr){
+			delete head;
+			head = tail = nullptr;
+		}
+		Node* list = head;
+		head = head-> next;
+		delete list;
+		head-> prev = nullptr;
+	}
 };
-void f(DoubleLinkedList2 copy) {
-}
-int main() {
+
+int main(){
+	ifstream file ("HW4b.txt");
 	DoubleLinkedList2 list;
-	list.addEnd(5);
-	list.removeEnd();
-	list.addStart(3);
-	list.removeStart();
-	for (int i = 0; i < 10; i++)
-		list.addEnd(i);
-	list.removeStart();
-	list.removeEnd();
-	cout << list << '\n';
-	//explicit copy	DoubleLinkedList2 list2 = list1;
-	DoubleLinkedList2 list3;
-	list3 = list; // operator =
-	f(list);
-	for (DoubleLinkedList2::Iterator i = list; !i; ++i)
-		cout << *i << ' ';
-	cout << '\n';
+	string inputString;
+	while(file >> inputString){
+		if(inputString == "OUTPUT")
+			cout << list << '\n';
+		else if (inputString == "ADD_FRONT"){
+			string inputNum;
+			file >> inputNum;
+			int position, start, step, end;
+
+			position = inputNum.find(":");
+			stringstream strTointa(inputNum.substr(0, position));
+			strTointa >> start;
+			inputNum.erase(0, position + 1);
+
+			position = inputNum.find(":");
+			stringstream strTointb(inputNum.substr(0, position));
+			strTointb >> step;
+			inputNum.erase(0, position + 1);
+
+			stringstream strTointc(inputNum);
+			strTointc >> end;
+			for (int i = start; i <= end; i += step){
+				list.addStart(i);
+			}
+		}
+		else if (inputString == "ADD_BACK"){
+			string inputNum;
+			file >> inputNum;
+			int position, start, step, end;
+
+			position = inputNum.find(":");
+			stringstream strTointa(inputNum.substr(0, position));
+			strTointa >> start;
+			inputNum.erase(0, position + 1);
+
+			position = inputNum.find(":");
+			stringstream strTointb(inputNum.substr(0, position));
+			strTointb >> step;
+			inputNum.erase(0, position + 1);
+
+			stringstream strTointc(inputNum);
+			strTointc >> end;
+
+			for (int i = start; i <= end; i += step)
+				list.addEnd(i);
+		}
+		else if (inputString == "REMOVE_BACK"){
+			string inputNum;
+			file >> inputNum;
+			int a;
+			stringstream strToint(inputNum);
+			strToint >> a;
+			for (int i = a; i > 0; i--)
+				list.removeEnd();
+		}
+		else if (inputString == "REMOVE_FRONT"){
+			string inputNum;
+			file >> inputNum;
+			int a;
+			stringstream strToint(inputNum);
+			strToint >> a;
+			for (int i = a; i > 0; i--)
+				list.removeStart();
+		}
+	}
+	file.close();
 }
