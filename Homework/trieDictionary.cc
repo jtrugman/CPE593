e483a86d5973a77e3dde7176b307976ec87752ea
Justin Trugman <jtrugman14@gmail.com>
2019-03-29 15:54:42 -0400

    upload

---
+++ Homework/trieDictionary.cc
@@ -0,0 +1,103 @@
+
+/*
+Author: Justin Trugman
+Collaborators: Dave Lehhman & KC Vasilas & Joe Puciloski 
+                Class Notes
+Homework Trie Dictionary 
+
+I pledge my honor that I abided by the Stevens Honor System
+
+*/
+
+#include<iostream>
+
+#include<string>
+#include<fstream>
+#include<sstream>
+
+using namespace std;
+
+class Trie {
+private:
+	class Node {
+	public:
+		Node* next[26];
+		bool isWord;
+		Node(){
+			for(int i = 0; i< 26; i++)
+				next[i] = nullptr;
+		}
+		
+	};
+
+
+	Node* root;
+
+
+public:
+	Trie() : root(new Node) {}
+	
+	void insert(string word){
+
+		Node* p = root;
+		for (int i = 0; i < word.length(); i++){
+
+			int index = (int)(word[i]) - 97;
+			if(p->next[index] == nullptr)
+				p->next[index] = new Node();
+			p = p->next[index];
+
+		}
+		p->isWord = true;
+	}
+	
+	void contains(string word){
+
+		Node* p = root;
+		for (int i = 0; i < word.length(); i++){
+
+			int index = (int)(word[i]) - 97;
+			if(p->next[index]==nullptr){
+				cout << "false" << '\n';
+				return;
+			}
+			p = p->next[index];
+		}
+
+		if(p->isWord)
+			cout << "true" << '\n';
+		else
+			cout << "false" << '\n';
+	}
+	
+	void containsPrefix(string word){
+
+		Node* p = root;
+		for (int i = 0; i < word.length(); i++){
+            
+			int index = (int)(word[i]) - 97;
+			if(p->next[index]==nullptr){
+				cout << "false" << '\n';
+				return;
+			}
+			p = p->next[index];
+		}
+
+		cout << "true" << '\n';
+	}
+};
+
+
+int main() {
+	Trie trie;
+	ifstream f ("dict.txt");
+	string dictionary;
+	while(f >> dictionary){
+		trie.insert(dictionary);
+	}
+	ifstream in ("hw5.txt");
+	string input;
+	while(in >> input){
+		trie.contains(input);
+	}
+}
