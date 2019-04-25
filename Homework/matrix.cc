e0954242b8b8e205935d522c4a6655ef583fd6ae
Justin Trugman <jtrugman14@gmail.com>
2019-04-25 15:43:32 -0400

    matrix

---
+++ Homework/matrix.cc
@@ -0,0 +1,78 @@
+/*
+Author: Justin Trugman
+Collaborators/References: Kirk Vasilas, David Lehman, Daniel Pinto, Class notes
+
+HW5: Matrix
+
+I pledge my honor that I abided by the Stevens Honor System
+*/
+
+
+#include <iostream>
+using namespace std;
+
+class Matrix {
+private:
+   double* m;
+   int rows, cols;
+   Matrix(int r, int c, char*) : rows(r), cols(c), m(new double[r*c]) {}
+public:
+   Matrix(int r, int c, double val = 0) : rows(r), cols(c), m(new double[rows*cols]) {
+      for(int i = 0; i < rows*cols; ++i)
+         m[i] = val;
+   }
+
+   ~Matrix() { delete [] m; }
+
+   Matrix(const Matrix& orig); 
+   Matrix& operator=(const Matrix& orig);
+   Matrix(Matrix&& orig) : rows(orig.rows), cols(orig.cols), m(orig.m) { 
+      orig.m = nullptr;
+   }
+
+   friend Matrix operator+(const Matrix& a, const Matrix& b) {
+      if(a.rows != b.rows || a.cols != b.cols)
+         throw "Bad size";
+      Matrix ans(a.rows, a.cols);
+      for(int i = 0; i < a.rows*a.cols; ++i)
+         ans.m[i] = a.m[i] + b.m[i];
+
+      return ans;
+   }
+      friend Matrix operator*(const Matrix& a, const Matrix &b) {
+      if(a.cols != b.rows)
+         throw "Size incorrect";
+
+      Matrix ans(a.rows, b.cols);
+      for(int k = 0; k < a.rows; k++) {
+         for(int j = 0; j < b.cols; j++) {
+            double dot = 0;
+            for(int i = 0; i < a.cols; i++)
+               dot += a(k,i) * b(i,j);
+            ans(k,j) = dot;
+         }
+      }
+      return ans;
+   }
+
+
+   friend Matrix operator-(const Matrix& a, const Matrix& b) {
+      if(a.rows != b.rows || a.cols != b.cols)
+         throw "Size incorrect";
+
+      Matrix ans(a.rows, a.cols);
+      for(int i = 0; i < a.rows*a.cols; ++i)
+         ans.m[i] = a.m[i] - b.m[i];
+
+      return ans;
+   }
+
+};
+
+int main() {
+  Matrix a(3,4,0.0);  // O(mn) = O(3*4)
+  Matrix b(4,2,1.0); //O(n*p) = O(4*2);
+  Matrix c = b; // make a copy O(np)
+  Matrix d = a + a;  //O(mn)
+  Matrix e = a * b;  //O(mnp)    O(n^3)
+}
